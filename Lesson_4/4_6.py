72dbadd002dd899df7edb007f984ae022f8f4838
OlgaVlasova <mumbelele@gmail.com>
2021-01-15 12:10:01 +0300

    Домашнее задание по уроку 4

---
+++ Lesson_4/4_6.py
@@ -0,0 +1,40 @@
+# Реализовать два небольших скрипта:
+# а) итератор, генерирующий целые числа, начиная с указанного,
+# б) итератор, повторяющий элементы некоторого списка, определенного заранее.
+# Подсказка: использовать функцию count() и cycle() модуля itertools.
+# Обратите внимание, что создаваемый цикл не должен быть бесконечным. Необходимо предусмотреть условие его завершения.
+# Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
+# Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.
+
+from itertools import count, cycle
+
+def script_a(begin=3, end=10):
+    for el in count(begin):
+        if el > end:
+            break
+        else:
+            yield el
+
+def script_b(value_list=None, quantity=5):
+    if value_list is None:
+        value_list = []
+    counter = 1
+    for el in cycle(value_list):
+        if counter > quantity:
+            break
+        counter += 1
+        yield el
+
+if __name__ == '__main__':
+
+    iter_a = script_a(15, 20)
+    print(iter_a)
+    for i in iter_a:
+        print(i)
+    print("Первая часть выполнена")
+
+    iter_b = script_b(['Привет,', 'как', 'дела', 'у', 'тебя', '?'], 15)
+    print(iter_b)
+    for i in iter_b:
+        print(i)
+    print("Вторая часть выполнена")
